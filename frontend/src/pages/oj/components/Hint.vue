92679cb9cc170e8d4f98120828b207fdb040c936
dana019@naver.com <102753070+meinnn@users.noreply.github.com>
2023-05-19 13:35:41 +0900

    gpt

--- frontend/src/pages/oj/components/Hint.vue
+++ frontend/src/pages/oj/components/Hint.vue
@@ -1,52 +1,103 @@
 <template>
   <div class="hint">
-    <b-button v-b-modal.modal-multi-1 id="popover-target-1" class="bg-warning">Hint</b-button>
-      <b-popover target="popover-target-1" triggers="hover" placement="bottomright">
+    <b-button v-if="step === 0"
+      v-b-modal.modal-multi-1 id="popover-target" class="bg-warning">Hint
+    </b-button>
+    <b-button v-else-if="step === 1"  @click="$bvModal.show('modal-multi-2')"
+      v-b-modal.modal-multi-2 id="popover-target" class="bg-warning">Hint
+    </b-button>
+    <b-button v-else-if="step === 2"  @click="$bvModal.show('modal-multi-3')"
+      v-b-modal.modal-multi-3 id="popover-target" class="bg-warning">Hint
+    </b-button>
+    <b-button v-else-if="step === 3"  @click="$bvModal.show('modal-multi-4')"
+      v-b-modal.modal-multi-4 id="popover-target" class="bg-warning">Hint
+    </b-button>
+    <b-button v-else-if="step === 4"  @click="$bvModal.show('modal-multi-5')"
+      v-b-modal.modal-multi-5 id="popover-target" class="bg-warning">Hint
+    </b-button>
+      <b-popover target="popover-target" triggers="hover" placement="bottomright">
         <template #title>Hint</template>
         <p class="my-1"><strong>1단계</strong>: <span class="text-danger">알고리즘 분류(Tag)</span> 공개</p>
         <p class="my-1"><strong>2단계</strong>: <span class="text-danger">사용 변수, 함수 인자</span> 공개</p>
         <p class="my-1"><strong>3단계</strong>: <span class="text-danger">의사 코드(Pseudo-code)</span> 공개</p>
         <p class="my-1"><strong>4단계</strong>: <span class="text-danger">해답</span> 공개</p>
       </b-popover>
+
     <b-modal id="modal-multi-1" title="Hint" @ok="$bvModal.show('modal-multi-2')" no-stacking>
       <p class="my-2">1단계 힌트를 보시겠습니까?</p>
-      <b-button v-b-modal.modal-multi-2>1단계 힌트 보기</b-button>
+      <b-button v-b-modal.modal-multi-2 @click="hintShowed()">1단계 힌트 보기</b-button>
     </b-modal>
     <b-modal id="modal-multi-2" title="1단계 힌트" no-stacking ok-only>
-      <p class="my-2">Algorithm Tag: <span class="text-danger">{{ tag.toUpperCase() }}</span></p>
-      <b-button v-b-modal.modal-multi-3 @click="getGPTResponse()">다음 단계 힌트 보기</b-button>
+      <p class="my-2">Algorithm Tag: <span class="text-danger">{{ (tag || '').toUpperCase() }}</span></p>
+      <b-button v-b-modal.modal-multi-3 @click="getGPTResponse(secondHint)">다음 단계 힌트 보기</b-button>
       <!-- <p v-else>{{ GPTResponseResult }}</p> -->
     </b-modal>
-    <b-modal id="modal-multi-3" title="2단계 힌트" ok-only>
+    <b-modal id="modal-multi-3" size="lg" title="2단계 힌트" no-stacking ok-only>
       <span v-if="showLoadingCircle"> L O A D I N G ... </span>
-      <p v-else>{{ GPTResponseResult }}</p>
+      <p v-else id="hint-message" v-html="renderedCodeBlock"></p>
+      <b-button v-if="showHintButton" @click="getGPTResponse(thirdHint), $bvModal.show('modal-multi-4')">다음 단계 힌트 보기</b-button>
+    </b-modal>
+    <b-modal id="modal-multi-4" size="lg" title="3단계 힌트" no-stacking ok-only>
+      <span v-if="showLoadingCircle"> L O A D I N G ... </span>
+      <p v-else id="hint-message">{{ GPTResponseResult }}</p>
+      <b-button v-if="showHintButton" @click="getGPTResponse(fourthHint), $bvModal.show('modal-multi-5')">다음 단계 힌트 보기</b-button>
+    </b-modal>
+    <b-modal id="modal-multi-5" size="lg" title="4단계 힌트" no-stacking ok-only>
+      <span v-if="showLoadingCircle"> L O A D I N G ... </span>
+      <p v-else id="hint-message" v-html="renderedCodeBlock"></p>
     </b-modal>
   </div>
 </template>
 
 <script>
 import fetch from 'node-fetch'
+import { marked } from 'marked'
 
 export default {
   name: 'Hint',
   data () {
     return {
+      step: 0,
       showHintButton: true,
       showLoadingCircle: false,
-      GPTResponseResult: ""
+      GPTResponseResult: "",
+      secondHint: '이 문제에 사용되는 변수와 함수 인자를 알려줘 "변수: ~~ 함수 인자: ~~ " 형태로 간단히 보여줘 정답은 보여주지 마',
+      thirdHint: '이 문제 해답의 pseudo-code를 알려줘 in English',
+      fourthHint: '이 문제 정답 코드만 보여줘'
     }
   },
   props: {
     tag: String,
-    problemDesc: String
+    problemDesc: String,
+    language: String
   },
   async mounted () {
   },
+  computed: {
+    renderedCodeBlock () {
+      // 마크다운 코드 블록을 HTML로 변환
+      const html = marked(this.GPTResponseResult);
+      return html;
+    },
+    processedDesc: function () {
+      return this.problemDesc.replace(/(<([^>]+)>)/gi, "")
+    }
+  },
   methods: {
-    getGPTResponse () {
-      console.log(this.problemDesc)
+    hintShowed () {
+      this.step++
+      console.log(this.step)
+    },
+    getGPTResponse (message) {
+      this.hintShowed()
+      console.log('!!' + this.problemDesc)
       this.showLoadingCircle = true
-      this.showHintButton = false;
+      this.showHintButton = false
+      let question = `${this.processedDesc} \n ${message}`
+      if (this.step === 4) {
+        question += ` 사용 언어는 ${this.language}`
+      }
+      console.log(question)
       fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
@@ -57,14 +108,17 @@ export default {
         },
         body: JSON.stringify({
           model: 'gpt-3.5-turbo',
-          messages: [{ role: 'user', content: `${this.problemDesc} \n 이 문제 풀어줘` }]
+          messages: [{ role: 'user', content: question }]
         })
       })
         .then((res) => res.json())
         .then((data) => {
-          this.showLoadingCircle = false;
+          this.showLoadingCircle = false
+          this.showHintButton = true
           // string 예쁘게 보이게 처리해야함
-          this.GPTResponseResult = data.choices[0].message.content
+          const responseText = data.choices[0].message.content
+          const formattedText = responseText.replace(/\\n/g, '<br />')
+          this.GPTResponseResult = formattedText
           console.log(this.GPTResponseResult)
         })
         .catch(err => console.warn(err))
@@ -72,3 +126,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  #hint-message {
+    white-space:pre-wrap;
+  }
+</style>
